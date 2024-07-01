5c929e04717dd198ff20906f4067200b768c94e2
yixin <1147852500@qq.com>
2024-07-01 23:44:21 +0200

    ✅ Add page cart

--- cart.php
+++ cart.php
@@ -1,20 +1,18 @@
 <?php 
     session_start();
     include "includes/utils.php";
-    $i18n = include('i18n/lang.php');
-    $lang = get_language();
 
 
     include "includes/db.php";
 
     echo create_page('template/index.html',[
-        'lang' => $lang,
-        'header_title' =>$i18n['title'][$lang],
-        'header_description' => $i18n['description'][$lang],
+
+        'header_title' =>'title',
+        'header_description' => 'description',
         'header_keywords' => "Shop, media, games",
         'header_author' =>"Author",
 
-        'skip_to_main' => $i18n['skip_to_main'][$lang],
+        'skip_to_main' => 'skip_to_main',
 
         'page_header' => create_page_header(),
         'page_main' => render(file_get_contents('template/cart.html'), [
